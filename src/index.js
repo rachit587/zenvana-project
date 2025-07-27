80a1684632a7feb90a03e0caf6d2ab05cdfa8cfa
rachit587 <110323927+rachit587@users.noreply.github.com>
2025-07-27 14:55:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="ZENVANA - Your AI Personal Finance Companion"
+    />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>ZENVANA</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
